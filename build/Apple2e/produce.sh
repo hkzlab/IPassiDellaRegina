b3220b1d8645ac421fc529688304fcd5a93ba444
Fabio Battaglia <tabaglio@posteo.net>
2026-01-11 20:43:36 +0100

    Add files to support Apple2e version

---
+++ build/Apple2e/produce.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+# All names of the tools used for accessing the disk images in the different
+# platforms are defined in a single config file in the parent directory:
+. ../config.sh
+
+# Assemble the disk image
+cp dsk/prodos.po ./queens_it.po
+java -jar $acjarfile -as ./queens_it.po QUEENS.SYSTEM < queens_it.bin
+java -jar $acjarfile -p ./queens_it.po EM.DRV 0 < dsk/a2e.auxmem.emd
+java -jar $acjarfile -p ./queens_it.po TEXT.DAT 0 < text_it.dat
+
+cp ../readme.txt .
+
+rm  AppleIIe_Queens_IT.zip
+zip -r AppleIIe_Queens_IT.zip queens_it.po readme.txt
+cp AppleIIe_Queens_IT.zip $ditdir
